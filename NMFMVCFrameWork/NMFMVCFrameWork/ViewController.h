a3914909b3df4d42c44d2cd76ed08a7e0cfb86be
Meng Fan <niumengfan@qichegou.com>
2017-05-23 17:53:34 +0800

    MVC 框架

---
+++ NMFMVCFrameWork/NMFMVCFrameWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NMFMVCFrameWork
+//
+//  Created by Meng Fan on 2017/5/23.
+//  Copyright © 2017年 Haowan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NMFMVCFrameWork/NMFMVCFrameWork/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NMFMVCFrameWork
+//
+//  Created by Meng Fan on 2017/5/23.
+//  Copyright © 2017年 Haowan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
